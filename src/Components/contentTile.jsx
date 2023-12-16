b86b83fd69fb5760f02cf77bc593f267efa177f9
gpnaveenram <96829467+gpnaveenram@users.noreply.github.com>
2023-12-17 02:45:22 +0530

    Update contentTile.jsx

--- src/Components/contentTile.jsx
+++ src/Components/contentTile.jsx
@@ -4,10 +4,10 @@ const ContentTile = () => {
     return (
         <div className="card-body">
             <div className="text-center">
-                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: "25rem" }} src="https://startbootstrap.github.io/startbootstrap-sb-admin-2/img/undraw_posting_photo.svg" alt="image"></img>
+                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: "25rem" }} src="https://startbootstrap.github.io/startbootstrap-sb-admin-2/img/undraw_posting_photo.svg" alt="disp"></img>
             </div>
             <p>Add some quality, svg illustrations to your project courtesy of unDraw, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-            <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
+            <a target="_blank" rel="noreferrer" href="https://undraw.co/">Browse Illustrations on
                 unDraw →</a>
         </div>
     );
